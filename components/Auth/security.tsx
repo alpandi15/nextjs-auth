2dfe1451ceafde62d5215b644a526e8293db13ff
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2022-03-23 14:43:04 +0700

    update security

--- components/Auth/security.tsx
+++ components/Auth/security.tsx
@@ -1,10 +1,8 @@
-import { NextComponentType, NextPageContext } from "next"
-import { getSession, GetSessionOptions } from "next-auth/client"
-import { Component } from "react"
+import { NextComponentType } from 'next'
+import { Component } from 'react'
 
 type LayoutingComponent = NextComponentType & {
   Layout?: any
-  // getLayout?: (page: any) => void
 }
 
 const getDisplayName = (Components: NextComponentType) => Components.displayName || Components.name || 'Component'
@@ -12,36 +10,6 @@ const getDisplayName = (Components: NextComponentType) => Components.displayName
 export const withAuthSync = (WrappedComponent: LayoutingComponent) => class extends Component {
   static displayName = `withAuthSync(${getDisplayName(WrappedComponent)})`
   static Layout = WrappedComponent?.Layout || null
-
-  // static async getInitialProps (ctx: NextPageContext) {
-  //   // console.log('Initial Server ', ctx)
-  //   const componentProps = WrappedComponent.getInitialProps
-  //   && (await WrappedComponent.getInitialProps(ctx))
-
-  //   console.log('ININTAL PROPS ', componentProps)
-  //   // let topNotifVerifiedAccount: boolean = false
-  //   // const { token, user } = await auth(ctx)
-  //   // if (!user?.emailVerified && !user?.phoneVerified) {
-  //   //   topNotifVerifiedAccount = true
-  //   // }
-  //   // return { ...componentProps, token, user, topNotifVerifiedAccount }
-  // }
-
-  // static async getServerSideProps (context: GetSessionOptions | any) {
-  //   const session = await getSession(context)
-  //   console.log('getServerSideProps ', context?.resolvedUrl);
-  //   if (!session) {
-  //     return {
-  //       redirect: {
-  //         destination: `/auth/login?redirect_to=${context?.resolvedUrl}`,
-  //         permanent: false
-  //       }
-  //     }
-  //   }
-  //   return {
-  //     props: { session }
-  //   }
-  // }
 
   constructor (props: any) {
     super(props)
